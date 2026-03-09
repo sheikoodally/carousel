17b2948891d2dff95ca6ac2f3e662304bdfd5991
sheik oodally <sheikoodally@hotmail.com>
2026-03-09 18:14:07 -0400

    carousel

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 
-const images = [
+const data = [
   { url: "./src/assets/images/001.jpg", alt: "Image 1" },
   { url: "./src/assets/images/002.jpg", alt: "Image 2" },
   { url: "./src/assets/images/003.jpg", alt: "Image 3" },
@@ -10,11 +10,29 @@ const images = [
   { url: "./src/assets/images/005.jpg", alt: "Image 5" },
 ];
 
+const NavigationHandler = ({
+  data,
+  handleNavClick,
+}: {
+  data: { url: string; alt: string }[];
+  handleNavClick: (index: number) => void;
+}) => {
+  return (
+    <div className="navigation-handler">
+      {data.map((item: { url: string; alt: string }, index: number) => (
+        <span key={index} onClick={() => handleNavClick(index)}>
+          {index + 1}
+        </span>
+      ))}
+    </div>
+  );
+};
+
 function App() {
   const [index, setIndex] = useState(0);
 
   const handleNext = () => {
-    if (index === images.length - 1) {
+    if (index === data.length - 1) {
       setIndex(0);
     } else {
       setIndex(index + 1);
@@ -23,17 +41,22 @@ function App() {
 
   const handlePrev = () => {
     if (index === 0) {
-      setIndex(images.length - 1);
+      setIndex(data.length - 1);
     } else {
       setIndex(index - 1);
     }
+  };
+
+  const handleNavClick = (index: number) => {
+    setIndex(index);
   };
 
   return (
     <>
       <div className="container">
         <h1>Image Carousel</h1>
-        <img src={images[index].url} alt={images[index].alt} className="carousel-image" />
+        <img src={data[index].url} alt={data[index].alt} className="carousel-image" />
+        <NavigationHandler data={data} handleNavClick={handleNavClick} />
         <div className="carousel-buttons">
           <button
             className="carousel-buttons-prev"
